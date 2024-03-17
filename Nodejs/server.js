f4faab711457abf729f2f9de6cc2b9af5f3f7a1d
NOOBE666 <outknock20@gmail.com>
2024-03-17 20:23:42 +0530

    Server created

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,5 +1,7 @@
+//Using express to create the server
 const express=require('express');
 const server=express();
+//server listening
 server.listen(3000,()=>{
     console.log('Server is listening at 3000');
 })
