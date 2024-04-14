ab2878e861a1fdc236fe5f105e51099f8bbe5a61
NOOBE666 <outknock20@gmail.com>
2024-04-14 10:45:57 +0530

    Mid term Eval

--- src/controller/page.js
+++ src/controller/page.js
@@ -16,20 +16,48 @@ export default class PageController{
         res.render('about');
     }
     business(req,res){
-        res.render('businesssign');
+        res.render('business');
     }
-    signup(req,res){
-        productmodelobj.addUser(req.body);
-        res.render('Home',{errormsg:error})
-    }
-    signin(req,res){
-        // console.log(req.body);
-        const ispresent=productmodelobj.findUser(req.body)
-        if(!ispresent){
-            error="User not found";
-            res.render('Home',{errormsg:error})
+    // signup(req,res){
+    //     productmodelobj.addUser(req.body);
+    //     res.render('Home',{errormsg:error})
+    // }
+    // signin(req,res){
+    //     // console.log(req.body);
+    //     const ispresent=productmodelobj.findUser(req.body)
+    //     if(!ispresent){
+    //         error="User not found";
+    //         res.render('Home',{errormsg:error})
+    //     }
+    //     else
+    //     res.render('Home',{errormsg:error});
+    // }
+    async signup(req, res) {
+        const user=await productmodelobj.addUser(req.body);
+        if(user){
+        try {
+          console.log(req.body);
+        //   await productmodelobj.addUser(req.body);
+          res.redirect('/');
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error adding user");
         }
-        else
-        res.render('Home',{errormsg:error});
     }
+    error="User already Exists";
+    res.redirect('/');
+      }
+      async signin(req, res) {
+        try {
+          console.log(req.body);
+          const user = await productmodelobj.findUser(req.body);
+          if (!user) {
+            error="User not found";
+          }
+          res.redirect('/');
+        } catch (error) {
+          console.error(error);
+          res.status(404).send("User not found");
+        }
+      }
 }
